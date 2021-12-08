7fc8bd603f15cd3a352ba1701c34e081d9f1ee9a
Mucumbe <mucumbeblandino@gmail.com>
2021-12-08 22:13:29 +0200

    Criando Domain SubRegiao

---
+++ src/main/java/co/mz/domain/SubRegiao.java
@@ -0,0 +1,21 @@
+package co.mz.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class SubRegiao {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into regiao (nome) values ('Norte')
+insert into regiao (nome) values ('Sul')
+insert into regiao (nome) values ('Este')
+insert into regiao (nome) values ('Oeste')
+
+insert into sub_regiao (nome) values ('SubRegiao1')
+insert into sub_regiao (nome) values ('SubRegiao2')
+insert into sub_regiao (nome) values ('SubRegiao3')
+insert into sub_regiao (nome) values ('SubRegiao4')
